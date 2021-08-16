4bcf9dc74cd1bcbdc894cd47ba94df8441fe1bf0
Demetrioo <matheus_d_vieira@estudante.sesisenai.org.br>
2021-08-16 15:06:40 -0300

    novo cadastro, login e suporte

---
+++ cadlog/login.js
@@ -0,0 +1,81 @@
+let container = document.querySelector('.input-container');
+let input = document.querySelector('#senha');
+let icon = document.querySelector('img');
+
+icon.addEventListener('click', function(){
+    container.classList.toggle('visible');
+    if(container.classList.contains('visible')){
+        icon.src = 'img/eye-off.svg';
+        input.type = 'text';
+    }else{
+        icon.src = 'img/eye.svg';
+        input.type = 'password';
+    }
+});
+
+// Javascript do Cadastro / login
+ 
+ 
+localStorage.login;
+localStorage.email;
+localStorage.senha;
+localStorage.confirme;
+ 
+localStorage.loginArray;
+localStorage.senhaArray;
+ 
+ 
+var auxEmailArray = new Array();
+var auxSenhaArray = new Array();
+ 
+function cadastrar(){
+ var email = document.getElementById('email').value;
+ var senha = document.getElementById('senha').value;
+ var confirme = document.getElementById('confirme').value;
+  
+ localStorage.setItem("email", email);//localStorage.login = l;
+//   localStorage.setItem("email", s);//localStorage.senha = s;
+ localStorage.setItem("senha", senha);//localStorage.senha = s;
+//   localStorage.setItem("confirme sua senha", f);//localStorage.senha = s;
+ 
+//   auxEmailArray.push(email);
+//   auxSenhaArray.push(senha);
+ 
+// //   //localStorage.loginArray = auxLoginArray;
+//   localStorage.setItem("emailArray", auxEmailArray);
+// //   //localStorage.senhaArray = auxSenhaArray;
+//   localStorage.setItem("senhaArray", auxSenhaArray);
+ 
+ //console.log(localStorage.loginArray, localStorage.senhaArray);
+ alert("Usuário cadastrado com sucesso\nEmail: "+localStorage.getItem("email") )
+}
+ 
+function logar(){
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+ 
+  
+  // if(l_inf == localStorage.login && s_inf == localStorage.senha){
+  // alert("Math");
+  // }
+ 
+  //var l = localStorage.loginArray.split(",");
+  var email = localStorage.getItem("loginArray").split(",");
+  //var s = localStorage.senhaArray.split(",");
+  var senha = localStorage.getItem("senhaArray").split(",");
+  
+  //console.log(l,s);
+  //alert("Login digitado: "+ l_inf + "\nSenha digitada: " + s_inf);
+  var logado = false;
+  for(var i=0; i<email.length; i++){
+  if(email == email[i] && senha == senha[i]){
+    alert("Usuário e senha existem no sistema");
+    logado = true;
+    window.open('suporte.html','janela');
+  }
+  }
+  if(logado == false){
+  alert("Usuário ou Senha não existem no sistema!");
+  }
+  //console.log("Login Storage:\n"+localStorage.loginArray, "\nSenhas Storage:\n"+localStorage.senhaArray);
+}
