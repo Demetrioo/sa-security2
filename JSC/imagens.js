2f44ac09ac2af551c9a6493900739acc4522ab91
Demetrioo <matheus_d_vieira@estudante.sesisenai.org.br>
2021-08-26 20:03:36 -0300

    produtos -js das imagens e css atualizado

---
+++ JSC/imagens.js
@@ -0,0 +1,74 @@
+//OUTLET
+
+let balaclava1 = document.getElementById('balaclava1')
+let balaclava2 = document.getElementById('balaclava2')
+let balaclava3 = document.getElementById('balaclava3')
+
+let conjunto1 = document.getElementById('conjuntout1')
+let conjunto1 = document.getElementById('conjuntoout2')
+let conjunto1 = document.getElementById('jaquetaout')
+
+let calcaout1 = document.getElementById('calcaout1')
+let calcaout1 = document.getElementById('calcaout2')
+let macacaoout = document.getElementById('macacaoout')
+
+let botaout1 = document.getElementById('botaout1')
+let botaout2 = document.getElementById('botaout2')
+let botaout3 = document.getElementById('botaout3')
+
+//CORRIDA
+
+let capaceteracing1 = document.getElementById('capaceteracing1')
+let capaceteracing2 = document.getElementById('capaceteracing2')
+let capaceteracing3 = document.getElementById('capaceteracing3')
+
+let macacaoracing1 = document.getElementById('macacaoracing1')
+let macacaoracing2 = document.getElementById('macacaoracing2')
+let macacaoracing3 = document.getElementById('macacaoracing3')
+
+let luvaracing1 = document.getElementById('luvaracing1')
+let luvaracing2 = document.getElementById('luvaracing2')
+let luvaracing3 = document.getElementById('luvaracing3')
+
+let sapatilharacing1 = document.getElementById('sapatilharacing1')
+let sapatilharacing2 = document.getElementById('sapatilharacing2')
+let sapatilharacing3 = document.getElementById('sapatilharacing3')
+
+//MOTO
+
+let capacetemoto1 = document.getElementById('capacetemoto1')
+let capacetemoto2 = document.getElementById('capacetemoto2')
+let capacetemoto3 = document.getElementById('capacetemoto3')
+
+let macacaomoto1 = document.getElementById('macacaomoto1')
+let macacaomoto2 = document.getElementById('macacaomoto2')
+let macacaomoto3 = document.getElementById('macacaomoto3')
+
+let luvamoto1 = document.getElementById('luvamoto1')
+let luvamoto2 = document.getElementById('luvamoto2')
+let luvamoto3 = document.getElementById('luvamoto3')
+
+let botamoto1 = document.getElementById('botamoto1')
+let botamoto2 = document.getElementById('botamoto2')
+let botamoto3 = document.getElementById('botamoto3')
+
+//KART
+
+let capacetekart1 = document.getElementById('capacetekart1')
+let capacetekart2 = document.getElementById('capacetekart2')
+let capacetekart3 = document.getElementById('capacetekart3')
+
+let macacaokart1 = document.getElementById('macacaokart1')
+let macacaokart2 = document.getElementById('macacaokart2')
+let macacaokart3 = document.getElementById('macacaokart3')
+
+let luvakart1 = document.getElementById('luvakart1')
+let luvakart2 = document.getElementById('luvakart2')
+let luvakart3 = document.getElementById('luvakart3')
+
+let sapatilhakart1 = document.getElementById('sapatilhakart1')
+let sapatilhakart2 = document.getElementById('sapatilhakart2')
+let sapatilhakart3 = document.getElementById('sapatilhakart3')
+
+
+
